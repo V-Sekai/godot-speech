29ff67858eb9b3f7b2422ee516f991735f5105a1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-28 06:50:17 -0700

    Update doc locaitons.

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
-# config.py
-
-
 def can_build(env, platform):
     return True
 
 
 def configure(env):
     pass
+
+
+def get_doc_classes():
+    return [
+        "Speech",
+        "SpeechDecoder",
+        "SpeechProcessor",
+    ]
+
+
+def get_doc_path():
+    return "doc_classes"
+	
